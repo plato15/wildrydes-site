2021fd49b8e1313f54d7520cedb602d991d87a96
Gabriel <plato@gabriels-mbp.myfiosgateway.com>
2020-11-01 17:35:15 -0500

    new

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_1wa0zXa0Z', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '25ng5kja9nce61bvri01pg6h3t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
