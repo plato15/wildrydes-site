31935abf3581d37e8ef3a09d8a9aac1eca97fbe9
Gabriel <plato@gabriels-mbp.myfiosgateway.com>
2020-11-01 17:30:43 -0500

    new

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_1wa0zXa0Z', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '25ng5kja9nce61bvri01pg6h3t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
